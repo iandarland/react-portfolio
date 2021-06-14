c1d71535072900462052dc4c375de214ee647f57
Ian Darland <iansmbpro@ians-mbp.lan>
2021-06-14 14:39:31 -0500

    updating with playdate

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -5,7 +5,7 @@ import "./style.css"
 function ProjectCard(props){
     return(
         <>
-        <div className="card col-md-3 col-12 col xl-2 col sm-4 mb-2" style={{width: "18rem", color: "black"}}>
+        <div className="card col-md-3 col-12 xl-2 col sm-4 mb-2" style={{width: "18rem", color: "black"}}>
             <img src={props.image} className="card-img-top" alt={props.name} />
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
